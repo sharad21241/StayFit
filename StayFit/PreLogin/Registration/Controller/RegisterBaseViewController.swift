bc7455d9a4070658e14de1401c02f967e9e369db
Sharad Gangurde <sharadg@winjit.com>
2023-01-09 15:30:46 +0530

    initial commit

---
+++ StayFit/PreLogin/Registration/Controller/RegisterBaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterBaseViewController.swift
+//  StayFit
+//
+//  Created by Sharad Gangurde on 09/01/23.
+//
+
+import UIKit
+
+class RegisterBaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
